e54a18cdfffaebdc93552216be46dbbbc7bd233e
wangchi <wangchi@meican.com>
2018-07-20 01:21:11 +0800

    clean code

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,21 +1,5 @@
 extern crate cryptopals_rs;
 
-use cryptopals_rs::set1::*;
-use cryptopals_rs::util;
-use cryptopals_rs::set1::BASE64_TABLE;
-
 fn main() {
-    util::read_file_to_vec_string("4.txt").iter().for_each ( |line| {
-        BASE64_TABLE.iter()
-                    .for_each (|b| {
-                        let r = xor_with_one_bytes(&hex_to_bytes(line), b);
-                        let s = String::from_utf8(r);
-                        match s {
-                            Ok(string) => {
-                                println!("{}:{}", string, *b as char);
-                            }
-                            _ => (),
-                        };
-                    });
-    });
+    println!("Hello world!");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,22 @@ pub mod util;
 
 #[cfg(test)]
 mod set1_test {
-    use super::*;
+    use super::set1::*;
+    use super::util::*;
     #[test]
     fn encode_base64() {
         let expected = String::from("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
         let hex_string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-        assert_eq!(set1::bytes_to_base64(&set1::hex_to_bytes(hex_string)), expected);
+        assert_eq!(bytes_to_base64(&hex_to_bytes(hex_string)), expected);
     }
 
     #[test]
     fn fixed_xor() {
         let expected = String::from("746865206b696420646f6e277420706c6179");
-        let left_bytes = set1::hex_to_bytes("1c0111001f010100061a024b53535009181c");
-        let right_bytes = set1::hex_to_bytes("686974207468652062756c6c277320657965");
+        let left_bytes = hex_to_bytes("1c0111001f010100061a024b53535009181c");
+        let right_bytes = hex_to_bytes("686974207468652062756c6c277320657965");
         assert_eq!(
-            set1::bytes_to_hex(&set1::xor_bytes(&left_bytes, &right_bytes)), 
+            bytes_to_hex(&xor_bytes(&left_bytes, &right_bytes)), 
             expected
         );
     }
@@ -26,19 +27,19 @@ mod set1_test {
     fn single_byte_xor() {
         let target_key = b'X';
         let target = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
-        let result = set1::xor_with_one_bytes(&set1::hex_to_bytes(target), &target_key);
+        let result = xor_with_one_bytes(&hex_to_bytes(target), &target_key);
         assert_eq!(String::from("Cooking MC's like a pound of bacon"), String::from_utf8(result).unwrap());
     }
 
     #[test]
     fn single_char_file() {
         let target_key = b'5';
-        let lines = util::read_file_to_vec_string("4.txt"); 
+        let lines = read_file_to_vec_string("4.txt"); 
         let result = lines
                      .iter()
                      .filter_map (|line| { 
                         String::from_utf8(
-                            set1::xor_with_one_bytes(&set1::hex_to_bytes(line), &target_key)
+                            xor_with_one_bytes(&hex_to_bytes(line), &target_key)
                         ).ok() 
                       })
                      .collect::<Vec<String>>();
