81569e75cbae3197441f1a4f58a7b41aff9fbaa9
wangchi <wangchi@meican.com>
2018-08-16 18:04:50 +0800

    split to set2

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,19 +6,5 @@ use openssl::symm::{decrypt, Cipher};
 use cryptopals_rs::set1::*;
 
 fn main() {
-    let cipher = Cipher::aes_128_ecb();
-    let data_content = read_file_to_vec_string("7.txt").concat();
-    let data = decode_base64(&data_content);
-
-    // let mut f = File::open("7.txt").expect("Unable to open '7.txt'");
-	// let mut contents = String::new();
-	// f.read_to_string(&mut contents).expect("Trouble reading '7.txt'");
-
-
-	// let contents = contents.lines().collect::<String>();
-	// let ciphertext = base64::decode(&contents);
-    let key = "YELLOW SUBMARINE".as_bytes();
-
-    let cipher_text = decrypt(cipher, key, None, &data).unwrap();
-    println!("{}", String::from_utf8(cipher_text).unwrap());
+    
 }
--- src/set1.rs
+++ src/set1.rs
@@ -178,14 +178,3 @@ pub fn aes128_ecb_encrypt(k: &[u8], data: &[u8]) -> Result<Vec<u8>, &'static str
     encrypt(cipher, k, None, data).map_err(|_| "encrypt ecb failed" )
 }
 
-pub fn pkcs7_padding(bytes: &[u8], block_size: usize) -> Vec<u8> {
-    let padding = (block_size - (bytes.len() % block_size)) as u8;
-    let mut with_padding = bytes.to_vec().clone();
-    if padding == 0 {
-        (0..block_size).for_each ( |_| { with_padding.push(block_size as u8) } );
-    } else {
-        (0..padding).for_each ( |_| { with_padding.push(padding) } );
-    }
-
-    return with_padding;
-}
